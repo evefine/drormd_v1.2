dc3bcc9bef03f2dcbd4970936704df1d9264cee1
EJ Fine <73449208+evefine@users.noreply.github.com>
2025-06-19 12:43:17 -0700

    Update rmsf.py for general

--- rmsf.py
+++ rmsf.py
@@ -18,29 +18,27 @@ def get_rmsf(top, ncs, align_sel, rmsf_sel):
         rmsf = RMSF(selection).run()
         return np.mean(rmsf.results.rmsf)
 
-root = '/oak/stanford/groups/rondror/projects/MD_simulations/amber/KOR/KOR_SK_PCP/v1/SK_tao1'
+lig_dirs = ['SK_tao1', 'PCP_tao']
 
-SK_top = '/oak/stanford/groups/rondror/projects/MD_simulations/amber/KOR/KOR_SK_PCP/v1/SK_tao1/prep/dabble/system_dabbled.psf'
-PCP_top = '/oak/stanford/groups/rondror/projects/MD_simulations/amber/KOR/KOR_SK_PCP/v1/PCP_tao/prep/dabble/system_dabbled.psf'
+reps = [6, 6]
 
-SK_ncs = []
-PCP_ncs = []
+align_sels = ['name CA and (protein and ((resid 58 to 69) or (resid 186 to 193) or (resid 286 to 294) or (resid 107 to 116) or (resid 135 to 144) or (resid 313 to 319) or (resid 227 to 231)))',
+'name CA and (protein and ((resid 58 to 72) or (resid 108 to 119) or (resid 135 to 142) or (resid 186 to 193) or (resid 227 to 235) or (resid 286 to 293) or (resid 312 to 320)))']
 
+rmsf_sels = ['resname SKET and not type H',
+'resname PCP and not type H']
 
-for i in range(1, 7):
-        SK_ncs.append(f'/oak/stanford/groups/rondror/projects/MD_simulations/amber/KOR/KOR_SK_PCP/v1/SK_tao1/run_{i}/summary_traj_w_eq_stride5.nc')
-        PCP_ncs.append(f'/oak/stanford/groups/rondror/projects/MD_simulations/amber/KOR/KOR_SK_PCP/v1/PCP_tao/run_{i}/summary_traj_w_eq_stride5.nc')
+root = '/oak/stanford/groups/rondror/projects/MD_simulations/amber/KOR/KOR_SK_PCP/v1/'
 
-align_SK = 'name CA and (protein and ((resid 58 to 69) or (resid 186 to 193) or (resid 286 to 294) or (resid 107 to 116) or (resid 135 to 144) or (resid 313 to 319) or (resid 227 to 231)))'
-align_PCP = 'name CA and (protein and ((resid 58 to 72) or (resid 108 to 119) or (resid 135 to 142) or (resid 186 to 193) or (resid 227 to 235) or (resid 286 to 293) or (resid 312 to 320)))'
+files = {}
 
-rmsf_sel_SK = 'resname SKET and not type H'
-rmsf_sel_PCP = 'resname PCP and not type H'
+for j, lig_dir in enumerate(lig_dirs):
+        files[lig_dir] = {}
+        files[lig_dir]['top'] = f'{root}/{lig_dir}/prep/dabble/system_dabbled.psf'
+        files[lig_dir]['trajs'] = [f'{root}/{lig_dir}/run_{x}/summary_traj_w_eq_stride5.nc' for x in range(1,reps[j]+1)]
 
-rmsf_SK = get_rmsf(SK_top, SK_ncs, align_SK, rmsf_sel_SK)
-rmsf_PCP = get_rmsf(PCP_top, PCP_ncs, align_PCP, rmsf_sel_PCP)
+rmsfs = {}
 
-print(f"""
-RMSF SK: {round(rmsf_SK,3)}
-RMSF PCP: {round(rmsf_PCP,3)}
-""")
+for i, lig_dir in enumerate(lig_dirs):
+        rmsfs[lig_dir] = get_rmsf(files[lig_dir]['top'], files[lig_dir]['trajs'], align_sels[i], rmsf_sels[i])
+        print(f'{lig_dir} RMSF: {round(rmsfs[lig_dir],2)}')
